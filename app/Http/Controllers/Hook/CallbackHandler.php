107563f1a671280b2cf424346b96ec47f4643134
Idris Shingarov <i.shingarov@petshop.ru>
2023-06-07 22:29:53 +0300

    update

--- app/Http/Controllers/Hook/CallbackHandler.php
+++ app/Http/Controllers/Hook/CallbackHandler.php
@@ -11,11 +11,12 @@ class CallbackHandler extends Controller
     public function make(Request $request)
     {
         if ($request->has('callback_query.reply_markup.inline_keyboard')) {
-            foreach ($request->input('callback_query.reply_markup.inline_keyboard')[0] as $markup) {
-                if ($markup['callback_data'] === 'get_skills') {
-                    return $this->getSkills($request->input('callback_query.from.id'));
-                }
-            }
+            $this->getSkills($request->input('callback_query.from.id'));
+//            foreach ($request->input('callback_query.reply_markup.inline_keyboard')[0] as $markup) {
+//                if ($markup['callback_data'] === 'get_skills') {
+//                    return $this->getSkills($request->input('callback_query.from.id'));
+//                }
+//            }
         }
     }
 
