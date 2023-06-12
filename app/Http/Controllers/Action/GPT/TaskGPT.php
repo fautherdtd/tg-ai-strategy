5f1ae506d7ebb02d5cdab1ca2c005d22a98fe099
Idris Shingarov <i.shingarov@petshop.ru>
2023-06-12 16:08:46 +0300

    update

--- app/Http/Controllers/Action/GPT/TaskGPT.php
+++ app/Http/Controllers/Action/GPT/TaskGPT.php
@@ -12,7 +12,8 @@ class TaskGPT
 {
     public int $chatID;
     public array $tasks = [
-        'analysis_market' => 'analysisMarket'
+        'analysis_market' => 'analysisMarket',
+        'make_strategy' => 'makeStrategy',
     ];
     /**
      * @param int $chatID
@@ -44,6 +45,33 @@ class TaskGPT
             'tasks' => implode(' ', [
                 '1. Проанализируй мне полностью рынок.',
                 '2. Исследуй рынок и помоги сегментировать целевую аудиторию.',
+            ])
+        ];
+
+        $gpt = new ChatGPT();
+        $result = $gpt->make(implode(' ', $placeholder));
+
+        $builder = new BuilderMessage($this->chatID);
+        return Sendler::send(
+          $builder->text($result)->buildText()
+        );
+
+    }
+
+    /**
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function makeStrategy(): mixed
+    {
+        $idea = ContextGPT::where('chat_id', $this->chatID)->first();
+        $placeholder = [
+            'role' => 'Ты - маркетолог. Твоя задача изучить мою идею, которая написана далее в кавычках',
+            'idea' => '"'. $idea->context . '"',
+            'tasks' => implode(' ', [
+                '1. Изучи всех конкурентов в этой области.',
+                '2. Напиши мне подробнее развитие моего бизнеса от 0 до первой прибыли.',
+                '3. Опиши мне эту стратегию развития максимально эффективно и подробно.',
             ])
         ];
 
