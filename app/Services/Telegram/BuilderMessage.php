c53c7f4328a738417302705097db9bf1dc8110ff
Idris Shingarov <i.shingarov@petshop.ru>
2023-06-12 03:11:46 +0300

    update

--- app/Services/Telegram/BuilderMessage.php
+++ app/Services/Telegram/BuilderMessage.php
@@ -41,7 +41,7 @@ class BuilderMessage
         ];
         if (!empty($buttons)) {
             $query['reply_markup'] = json_encode([
-                'inline_keyboard' => [$buttons]
+                'inline_keyboard' => $buttons
             ]);
         }
         return $query;
